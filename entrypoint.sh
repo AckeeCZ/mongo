996dc5b3e3c6dda73bb7d96755403b70f89ce177
Josef Gattermayer <josef.gattermayer@ackee.cz>
2016-04-06 11:47:59 +0200

    added comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,14 @@ if [ -z "$S3_ACCESS_KEY" -a -z "$S3_SECRET_KEY" -a -z "$S3_URL" -a -z "$MONGO_UR
 	exit 1
 fi
 
-#set S3
+#set S3 config
 sed -i "s/%%S3_ACCESS_KEY%%/$S3_ACCESS_KEY/g" /root/.s3cfg
 sed -i "s/%%S3_SECRET_KEY%%/$S3_SECRET_KEY/g" /root/.s3cfg
 
 #verify S3 config
 s3cmd ls "s3://$S3_URL" > /dev/null
 
-# add cron job
+# add a cron job
 echo "0 2 * * * root rm -rf /tmp/dump && mongodump -h $MONGO_URL -u root -p $MONGO_ROOT_PASSWORD --gzip -o /tmp/dump && s3cmd sync /tmp/dump s3://$S3_URL/ && rm -rf /tmp/dump" >> /etc/crontab
 crontab /etc/crontab
 
