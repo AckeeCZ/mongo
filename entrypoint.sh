7ea0c3b7f78c559342821abda71f563fe1a456c7
Marek Bartík <bartimar6@gmail.com>
2016-04-07 15:56:29 +0200

    tuning - bash nazi

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 set -eo pipefail
 
 #verify variables
-if [ -z "$S3_ACCESS_KEY" -a -z "$S3_SECRET_KEY" -a -z "$S3_URL" -a -z "$MONGO_URL" ]; then
-	echo >&2 'Backup inforamtion is not complete. You need to specify S3_ACCESS_KEY, S3_SECRET_KEY, S3_URL and MONGO_URL.'
+if [ -z "$S3_ACCESS_KEY" -o -z "$S3_SECRET_KEY" -o -z "$S3_URL" -o -z "$MONGO_URL" ]; then
+	echo >&2 'Backup information is not complete. You need to specify S3_ACCESS_KEY, S3_SECRET_KEY, S3_URL and MONGO_URL.'
 	exit 1
 fi
 
 #set S3 config
-sed -i "s/%%S3_ACCESS_KEY%%/$S3_ACCESS_KEY/g" /root/.s3cfg
-sed -i "s/%%S3_SECRET_KEY%%/$S3_SECRET_KEY/g" /root/.s3cfg
+sed -i "s#%%S3_ACCESS_KEY%%#$S3_ACCESS_KEY#g" /root/.s3cfg
+sed -i "s#%%S3_SECRET_KEY%%#$S3_SECRET_KEY#g" /root/.s3cfg
 
 #verify S3 config
 s3cmd ls "s3://$S3_URL" > /dev/null
