24566f573bce2cea6409ac4120d305e5e1260c2d
Josef Gattermayer <josef.gattermayer@ackee.cz>
2016-04-08 14:29:23 +0200

    backup interval set to every hour

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ sed -i "s#%%S3_SECRET_KEY%%#$S3_SECRET_KEY#g" /root/.s3cfg
 s3cmd ls "s3://$S3_URL" > /dev/null
 
 # add a cron job
-echo "0 2 * * * root rm -rf /tmp/dump && mongodump -h $MONGO_URL -u root -p $MONGO_ROOT_PASSWORD --gzip -o /tmp/dump && s3cmd sync /tmp/dump s3://$S3_URL/ && rm -rf /tmp/dump" >> /etc/crontab
+echo "2 * * * * root rm -rf /tmp/dump && mongodump -h $MONGO_URL -u root -p $MONGO_ROOT_PASSWORD --gzip -o /tmp/dump && s3cmd sync /tmp/dump s3://$S3_URL/ && rm -rf /tmp/dump" >> /etc/crontab
 crontab /etc/crontab
 
 exec "$@"
